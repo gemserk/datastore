b8b3786b2abacb053a3e04d05daf91ab7a2b75cb
Ruben Garat <ruben.garat@gemserk.com>
2010-05-14 14:30:22 +0000

    first import
    git-svn-id: https://svn.gemserk.com/utils/datastore/trunk/datastore@14 f74af82c-f8a9-4159-9544-6b194567659e

---
+++ src/main/java/com/gemserk/datastore/DataStore.java
@@ -0,0 +1,16 @@
+package com.gemserk.datastore;
+
+import java.util.Collection;
+import java.util.Set;
+
+public interface DataStore {
+	
+	/**
+	 * @param data
+	 * @return the id of the entry
+	 */
+	String submit(Data data);
+	
+	Collection<Data> get(Set<String> tags);
+
+}
