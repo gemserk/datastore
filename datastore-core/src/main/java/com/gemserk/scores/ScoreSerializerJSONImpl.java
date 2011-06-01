ebf32e891a2d716b075bf712edb95eee7e545e7e
Ariel Coppes <ariel.coppes@gemserk.com>
2011-06-01 18:26:01 -0300

    added map<string,object> gson fix

--- datastore-core/src/main/java/com/gemserk/scores/ScoreSerializerJSONImpl.java
+++ datastore-core/src/main/java/com/gemserk/scores/ScoreSerializerJSONImpl.java
@@ -6,17 +6,40 @@ import java.util.Collection;
 import java.util.Map;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.reflect.TypeToken;
 
 public class ScoreSerializerJSONImpl implements ScoreSerializer {
 
-	private final Gson gson = new Gson();
+	private final Gson gson;
+
+	public ScoreSerializerJSONImpl() {
+		gson = new GsonBuilder().registerTypeAdapter(Object.class, new JsonDeserializer<Object>() {
+			@Override
+			public Object deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+				JsonPrimitive value = json.getAsJsonPrimitive();
+				if (value.isBoolean()) {
+					return value.getAsBoolean();
+				} else if (value.isNumber()) {
+					return value.getAsNumber();
+				} else {
+					return value.getAsString();
+				}
+			}
+		}).create();
+	}
 
 	@Override
 	public Collection<Score> parse(String scoresString) {
 		if ("".equals(scoresString) || scoresString == null)
 			return new ArrayList<Score>();
-		Type collectionType = new TypeToken<Collection<Score>>(){}.getType();
+		Type collectionType = new TypeToken<Collection<Score>>() {
+		}.getType();
 		return gson.fromJson(scoresString, collectionType);
 	}
 
