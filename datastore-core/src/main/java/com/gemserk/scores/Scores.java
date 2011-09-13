2d95e5de8df9984b5039ae319c03b9039f7237ec
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-13 00:26:57 -0300

    improved a bit Scores javadocs

--- datastore-core/src/main/java/com/gemserk/scores/Scores.java
+++ datastore-core/src/main/java/com/gemserk/scores/Scores.java
@@ -13,19 +13,30 @@ public interface Scores {
 	 * Submits a new score, returns the id of the score.
 	 * 
 	 * @param score
-	 * @return the id of the entry
+	 *            The score to submit
+	 * @return the id of the score
 	 */
 	String submit(Score score);
 
+	/**
+	 * Submits a new score using the profilePrivateKey to identify the score's player profile.
+	 * 
+	 * @param profilePrivateKey
+	 *            The private key of the profile of the player.
+	 * @param score
+	 *            The score to submit.
+	 * @return The id of the score
+	 */
 	String submit(String profilePrivateKey, Score score);
 
 	/**
 	 * Returns scores tagged with the given tags ordered by points descending.
 	 * 
 	 * @param tags
+	 *            A collection of tags used to filter the scores.
 	 * @param limit
 	 *            the number of results
-	 * @return
+	 * @return A collection of scores matching the search criteria.
 	 */
 	Collection<Score> getOrderedByPoints(Set<String> tags, int limit, boolean ascending);
 
@@ -40,7 +51,7 @@ public interface Scores {
 	 *            If the scores should be ordered ascending or not.
 	 * @param range
 	 *            The range of the scores, day for today scores, week for scores of this week, etc.
-	 * @return
+	 * @return A collection of scores matching the search criteria.
 	 */
 	Collection<Score> getOrderedByPoints(Set<String> tags, int limit, boolean ascending, Range range);
 
