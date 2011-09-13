37fc47e16293a64b3732ec3c7aa158ae2b25cdd7
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-13 14:31:54 -0300

    added inmemory implementaiton of Profiles interface to be used for testing purposes for example

---
+++ datastore-core/src/main/java/com/gemserk/datastore/profiles/ProfilesMemoryImpl.java
@@ -0,0 +1,43 @@
+package com.gemserk.datastore.profiles;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class ProfilesMemoryImpl implements Profiles {
+	
+	private HashMap<String, Profile> profileMap = new HashMap<String, Profile>();
+	private Random random = new Random();
+
+	@Override
+	public Profile register(String name, boolean guest) {
+		Profile profile = new Profile(generatePrivateKey(), generatePublicKey(), name, guest);
+		profileMap.put(profile.getPrivateKey(), profile);
+		return profile;
+	}
+
+	private String generatePublicKey() {
+		return "privateKey-" + getRandomNumber();
+	}
+
+	private String generatePrivateKey() {
+		return "publicKey-" + getRandomNumber();
+	}
+
+	private long getRandomNumber() {
+		return (random.nextInt() % 89999) + 10000;
+	}
+
+	@Override
+	public Profile update(Profile profile) {
+		if (!profile.isGuest()) 
+			throw new RuntimeException("can't update a non guest profile");
+		
+		Profile savedProfile = profileMap.get(profile.getPrivateKey());
+		
+		savedProfile.setName(profile.getName());
+		savedProfile.setGuest(false);
+		
+		return savedProfile;
+	}
+
+}
